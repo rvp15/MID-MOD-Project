89fb2ad0f316aa2ef1fe0e5502cda4215098a576
rvp15 <vedhagithub@gmail.com>
2022-10-28 17:26:46 -0400

    Project progress

--- thespace-app/src/pages/Liked.js
+++ thespace-app/src/pages/Liked.js
@@ -1,39 +1,73 @@
-import React from 'react'
+import React from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setremoveliked } from "../slices/likedSlice";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 import {
   MDBCard,
   MDBCardBody,
   MDBCardText,
   MDBCardImage,
-  MDBBtn
-} from 'mdb-react-ui-kit';
-
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 function Liked() {
+  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
-  const {likeddata} = useSelector((state)=>state.liked)
-console.log(likeddata)
+  const { likeddata } = useSelector((state) => state.liked);
+  console.log(likeddata);
 
   return (
-    <div className='likedcontainer'>
-      {likeddata?.map((item)=>{
-       return(
-        <MDBCard>
-        <div className="eachcardliked" >
-        <MDBCardImage className="image" src={item.url} position='top'/>
-        <MDBCardBody>
-        <MDBCardText>{item.title}</MDBCardText>
-        <MDBBtn onClick={() => dispatch(setremoveliked(item))}>Remove</MDBBtn>
-        </MDBCardBody>
-      </div>
-      </MDBCard> 
-       )
+    <div className="likedcontainer">
+      {likeddata?.map((item) => {
+        return (
+          <MDBCard>
+            <div className="eachcardliked">
+              <MDBCardImage className="image" src={item.url} position="top" />
+              <MDBCardBody>
+                <MDBCardText>{item.title}</MDBCardText>
+                <div className="buttonsliked">
+                <Button
+                  className="detailbtn"
+                  variant="primary"
+                  onClick={() => setShow(true)}
+                >
+                  {" "}
+                  Details
+                </Button>
+                <MDBBtn
+                  className="detailbtn"
+                  onClick={() => dispatch(setremoveliked(item))}
+                >
+                  Remove
+                </MDBBtn>
+                </div>
+              </MDBCardBody>
+            </div>
+
+            <Modal
+              show={show}
+              onHide={() => setShow(false)}
+              dialogClassName="modal-90w"
+              aria-labelledby="example-custom-modal-styling-title"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="example-custom-modal-styling-title">
+                  {item.title}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>{item.explanation}</p>
+              </Modal.Body>
+            </Modal>
+          </MDBCard>
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default Liked
+export default Liked;
